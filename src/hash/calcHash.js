3f37ecb1866a4050af3ebae333c73fcccbdb756c
sodapng <sodapng@ya.ru>
2022-06-05 16:05:43 +0400

    refactor: calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,7 +1,7 @@
 import { createHash } from 'node:crypto'
 import { createReadStream } from 'node:fs'
 import { resolve } from 'node:path'
-import { pipeline } from 'node:stream'
+import { pipeline } from 'node:stream/promises'
 import getDirname from '../utils/dirname.js'
 
 const __dirname = getDirname(import.meta.url)
@@ -11,14 +11,21 @@ export const calculateHash = async () => {
   const rs = createReadStream(fileToPath)
   const hash = createHash('sha256')
 
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
     // rs.on('error', (err) => reject(err))
     //   .on('data', (data) => hash.update(data))
     //   .on('end', () => resolve(hash.digest('hex')))
 
-    pipeline(rs, hash, (err) =>
-      err ? reject(err) : resolve(hash.digest('hex'))
-    )
+    // pipeline(rs, hash, (err) =>
+    //   err ? reject(err) : resolve(hash.digest('hex'))
+    // )
+
+    try {
+      await pipeline(rs, hash)
+      resolve(hash.digest('hex'))
+    } catch (error) {
+      reject(error)
+    }
   })
 }
 
